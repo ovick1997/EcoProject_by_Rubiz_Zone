d73fd6f717d55522099b06e9ff83b3dbb16149ce
Md Shorov Abedin <mdovick1952@gmail.com>
2019-09-09 23:48:05 +0600

    login method create in logincontroller

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
- 
+use Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -33,5 +33,20 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        if (Auth::guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ], $request->get('remember'))) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        return back()->withInput($request->only('email', 'remember'));
     }
 }
